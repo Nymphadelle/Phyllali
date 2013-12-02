f53ab719ba47df76425c87f17ed7264690bd0b4a
Elise <emf.richard@gmail.com>
2013-12-02 22:13:29 +0100

    Classes

---
+++ Classes/Produit.php
@@ -0,0 +1,29 @@
+
+<?php
+
+require_once 'Connect.php';
+
+class Produit extends Connect{
+
+
+
+	//renvoie la liste des produits
+	public function getProduits(){
+		$sql ='select * from PRODUIT';
+		$produits = $this->executerRequete($sql);
+		$tab = array();
+		while(odbc_fetch_row($produits)){
+			$tableau = array();		
+			for($i=1;$i<=odbc_num_fields($produits);$i++){
+				$tableau[odbc_field_name ( $produits, $i )] =odbc_result($produits,$i);
+			}
+			array_push($tab, $tableau);
+		}
+		return $tab;
+	}
+	
+		
+}
+
+
+?>
